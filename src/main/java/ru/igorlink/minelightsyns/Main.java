3ccb69079c83daf13d60fdc5765af02ba97f154e
Xaver <a@gavr.xyz>
2020-08-12 12:50:19 +0300

    Add comments in Main.java

--- src/main/java/ru/igorlink/minelightsyns/Main.java
+++ src/main/java/ru/igorlink/minelightsyns/Main.java
@@ -7,29 +7,38 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            return true;
-        } else if (args.length > 1) {
-            sender.sendMessage("E0");
-            return true;
-        } else {
-            String username = args[0];
-            if (this.getServer().getPlayer(username) == null) {
-                sender.sendMessage("E1");
-                return true;
-            } else {
-                Player player = this.getServer().getPlayer(username);
-                Location player_cords = player.getLocation();
-                Location top_block_cords = new Location(player_cords.getWorld(), player_cords.getX(), player_cords.getY() + 1.0D, player_cords.getZ());
-                if (top_block_cords != null) {
-                    int light_level = top_block_cords.getBlock().getLightLevel();
-                    sender.sendMessage(String.valueOf(light_level));
-                }
 
-                return true;
-            }
+public class Main extends JavaPlugin implements Listener {
+    //При вызове нашей команды
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        //Если у команды больше одного аргумента - это ошибка (у нашей команды должен быть один аргумент - ник игрока), возвращаем код E0
+        if (args.length > 1) {
+            sender.sendMessage("E0");
+            return true; //Выходим
+
         }
+
+        //Если игрок с указанным ником не найден, возвращаем код ошибки E1
+        String username = args[0];
+        if (getServer().getPlayer(username) == null) {
+            sender.sendMessage("E1");
+            return true; //Выходим
+
+        }
+
+        Player player = getServer().getPlayer(username); //Получаем объект игрока
+        Location player_cords = player.getLocation(); //Получаем координаты игрока
+        Location top_block_cords = new Location(player_cords.getWorld(), player_cords.getX(), player_cords.getY() + 1, player_cords.getZ()); //Координаты блока с нашей головой - это на 1 выше, чем координаты игрока
+        //Если успешно получили координаты игрока...
+        if (top_block_cords != null) {
+            int light_level = top_block_cords.getBlock().getLightLevel(); //Получаем уровень освещенности блока с головой игрока
+            sender.sendMessage(String.valueOf(light_level)); //Отправляем этот уровень освещенности отправителю команды (в нашем проекте это будет программа на питоне, которая будет вызывать команды через rcon)
+        }
+
+        return true; //Выходим
+
     }
+
 }
