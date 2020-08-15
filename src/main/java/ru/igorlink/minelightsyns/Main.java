6f236b32ae4f2db690bcb401b952d24357ea395a
JolyJDIA <jolyjdia@gmail.com>
2020-08-15 10:08:20 +0300

    почистил импорты

--- src/main/java/ru/igorlink/minelightsyns/Main.java
+++ src/main/java/ru/igorlink/minelightsyns/Main.java
@@ -1,11 +1,7 @@
 package ru.igorlink.minelightsyns;
 
 import org.bukkit.Location;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
